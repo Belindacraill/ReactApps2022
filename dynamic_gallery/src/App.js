8408f3180ca3a355570fb3a35cc243c6b14e1797
Belinda Craill <36764613+Belindacraill@users.noreply.github.com>
2022-09-09 13:42:25 +0200

    react Apps

---
+++ dynamic_gallery/src/App.js
@@ -0,0 +1,13 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Gallery from "./portfolio-gallery/Gallery";
+function App() {
+  return (
+    <div className="App">
+      <Gallery />
+    </div>
+  );
+}
+
+export default App;
+
+
---
+++ to_do_list/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import TodoApp from "./todo/todoApp";
+class App extends React.Component {
+  render() {
+    return <TodoApp />;
+  }
+}
+export default App;
